d553252f0d71ec8c131f64da7193202b31dc8721
joelsondsbreis <joelsondsbreis@gmail.com>
2020-04-28 18:31:29 -0300

    Controller do agendamento de serviços criado.

---
+++ modulo2/src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  // método que vai ser chamado automaticamente pelo sequelize
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
