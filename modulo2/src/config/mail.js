41d93a544edaacdddcc728ea3ffe0e7cd37c9878
joelsondsbreis <joelsondsbreis@gmail.com>
2020-05-04 11:12:06 -0300

    Configurando nodemaile para envios de emails.

---
+++ modulo2/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '583191e8168388',
+    pass: 'f8b88cc041dd91',
+  },
+  default: {
+    from: 'Joelson Reis <noreply@gobarber.com>',
+  },
+};
--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -26,10 +26,12 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointment', AppointmentController.index);
 routes.post('/appointment', AppointmentController.store);
+routes.delete('/appointment/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
